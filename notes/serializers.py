d1e14b7a237bbdb857281bb4820ce302a36f7b41
Callum <callummorgan666@gmail.com>
2024-07-02 15:54:30 +0100

    add create and update functions to user serializer

--- notes/serializers.py
+++ notes/serializers.py
@@ -20,5 +20,26 @@ class UserSerializer(serializers.ModelSerializer):
         fields = ['id', 'username', 'email', 'password']
         extra_kwargs = {'password': {'write_only': True}}
 
+    def create(self, validated_data):
+        try:
+            user = User.objects.create_user(
+                username=validated_data['username'],
+                email=validated_data['email'],
+                password=validated_data['password']
+            )
+            Profile.objects.create(user=user, name=user.username, email=user.email)
+            return user
+        except ValidationError as e:
+            raise serializers.ValidationError({'detail': str(e)})
+        except Exception as e:
+            raise serializers.ValidationError({'detail': str(e)})
+
+    def update(self, instance, validated_data):
+        password = validated_data.pop('password', None)
+        instance = super().update(instance, validated_data)
+        if password:
+            instance.set_password(password)
+            instance.save()
+        return instance
 
     
