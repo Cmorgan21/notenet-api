dbcb9f4ebbcc29ca97abe8f7d3a5a8e5590b750a
Callum <callummorgan666@gmail.com>
2025-06-10 21:19:08 +0100

    add models to category

--- categories/models.py
+++ categories/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+class Category(models.Model):
+    """
+    A model representing a catergory
+    """
+    name = models.CharField(max_length=100)
+    owner = models.ForeignKey(User, on_delete=models.CASCADE, related_name='categories')
+
+    def __str__ (self):
+        return f'{self.name}'
+    
--- notes/models.py
+++ notes/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
+from categories.models import Category
 
 # Create your models here.
 
@@ -12,6 +13,7 @@ class Note(models.Model):
     created_on = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
     author = models.ForeignKey(User, on_delete=models.CASCADE, related_name='notes')
+    category = models.ForeignKey(Category, on_delete=models.SET_NULL, null=True, blank=True, related_name='notes')  # ✅ New field
 
     def __str__(self):
         return f'{self.title} by {self.author}'
