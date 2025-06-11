12c09c237959c2d28a62abad12e13046a0a37745
Callum <callummorgan666@gmail.com>
2025-06-11 17:23:35 +0100

    add functionality to category api

--- categories/views.py
+++ categories/views.py
@@ -1,3 +1,16 @@
-from django.shortcuts import render
+from rest_framework import generics, permissions
+from .models import Category
+from .serializers import CategorySerializer
 
-# Create your views here.
+class CategoryListCreateView(generics.ListCreateAPIView):
+    """
+    List all categories or create a new category.
+    """
+    serializer_class = CategorySerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+    def get_queryset(self):
+        return Category.objects.filter(owner=self.request.user).order_by('-created_on')
+
+    def perform_create(self, serializer):
+        serializer.save(owner=self.request.user)
