840b611f872bef679260d40e657ac64be30dc054
Callum <callummorgan666@gmail.com>
2025-06-11 17:26:51 +0100

    add url endpoints to category app

---
+++ categories/urls.py
@@ -0,0 +1,7 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('categories/', views.CategoryListCreateView.as_view(), name='category-list-create'),
+    path('categories/<int:pk>/', views.CategoryDetailView.as_view(), name='category-detail'),
+]
